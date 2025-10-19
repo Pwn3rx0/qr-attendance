6e53180f94d7783291b27a5fb78d3bb13df76bc7
Andrew Mamdouh <134093499+Pwn3rx0@users.noreply.github.com>
2025-10-19 23:52:28 +0300

    Delete src directory

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import QRAttendanceApp from "./QRAttendanceApp";
-
-function App() {
-  return <QRAttendanceApp />;
-}
-
-export default App;
--- src/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/qr-attendance/', // نفس اسم المستودع
-  plugins: [react()],
-})
